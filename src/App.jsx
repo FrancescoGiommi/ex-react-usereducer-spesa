8af3be3eb651c7840724500d2dce864afe8c69ba
Francesco <you@example.com>
2025-03-18 14:43:54 +0100

    Add milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,22 @@ const products = [
         Prezzo
 
 Obiettivo: Vedere un elenco leggibile di tutti i prodotti con nome e prezzo. */
+
+//! Milestone 2: Aggiungere prodotti al carrello
+
+/* Aggiungi uno stato locale addedProducts (inizialmente un array vuoto) per rappresentare i prodotti nel carrello.
+    Per ogni prodotto della lista, aggiungi un bottone "Aggiungi al carrello":
+        Al click del bottone, usa una funzione addToCart per:
+            Aggiungere il prodotto al carrello se non è già presente, con una proprietà quantity = 1.
+        Se il prodotto è già nel carrello, ignora l’azione.
+    Sotto alla lista dei prodotti, mostra una lista dei prodotti nel carrello se addedProducts contiene almeno un elemento.
+        Per ogni prodotto nel carrello, mostra:
+            Nome
+            Prezzo
+            Quantità
+
+Obiettivo: L’utente può aggiungere prodotti al carrello e vedere una lista dei prodotti aggiunti. */
+
 function App() {
   const products = [
     { name: "Mela", price: 0.5 },
@@ -33,6 +49,19 @@ function App() {
 
     { name: "Pasta", price: 0.7 },
   ];
+  const [addedProducts, setAddedProducts] = useState([]);
+  function addToCart(product) {
+    const productCard = addedProducts.some((p) => p.name === product.name);
+    if (productCard) {
+      return;
+    } else {
+      setAddedProducts([
+        ...addedProducts,
+        (product = { ...product, quantity: 1 }),
+      ]);
+    }
+    return productCard;
+  }
   return (
     <>
       <h1>Lista dei prodotti</h1>
@@ -43,6 +72,20 @@ function App() {
               {product.name} {""}
               {product.price.toFixed(2)}€
             </p>
+            <button onClick={() => addToCart(product)}>
+              Aggiungi al carrello
+            </button>
+          </li>
+        ))}
+      </ul>
+      <h2>Prodotti nel carrello</h2>
+      <ul>
+        {addedProducts.map((product, index) => (
+          <li key={index}>
+            {product.name} {product.price.toFixed(2)}
+            {"€"}
+            {"" + "-" + ""}
+            {`Quantità: ${product.quantity}`}
           </li>
         ))}
       </ul>
